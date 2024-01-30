1205b01f868503d5d45f309899dfd99f276bd8d8
b1ancoi <157384877+b1ancoi@users.noreply.github.com>
2024-01-30 22:01:40 +0200

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -189,6 +189,13 @@ class TestProductRoutes(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(len(data), 5)
+    def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
 
     ######################################################################
     # Utility functions
